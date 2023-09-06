a03aeec12cd7b10c72549dc318a85bb68a67fe2d
Prashant Singh <prashant6.singh@airtelbank.com>
2023-09-06 15:39:53 +0530

    initial commit

---
+++ src/main/java/com/dockerdemo/sample/docker/ci/cd/pipeline/SampleDockerCiCdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.dockerdemo.sample.docker.ci.cd.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDockerCiCdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDockerCiCdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockerdemo/sample/docker/ci/cd/pipeline/SampleDockerCiCdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.dockerdemo.sample.docker.ci.cd.pipeline;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class SampleDockerCiCdPipelineApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
